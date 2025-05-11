da77aac534f805ac7f57137882c43f8bfdc50cdd
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2025-05-11 21:29:13 +0700

    oke

--- index.php
+++ index.php
@@ -2,33 +2,33 @@
 session_start();
 include "./pages/db.php";
 
-// Ambil URI setelah domain
-$request = trim(str_replace("/KosPelitaHarapan", "", parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH)), "/");
+// Ambil URI setelah "/KosPelitaHarapan"
+$request = trim(str_replace("/kosPelitaHarapan", "", parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH)), "/");
 
-// Definisikan route statis
+// Route statis
 $routes = [
-    "" => "pages/home.php", 
+    "" => "pages/home.php",
     "home" => "pages/home.php",
-    "koneksi" => "pages/db.php", 
+    "koneksi" => "pages/db.php",
     "info" => "pages/info.php",
     "login" => "pages/login.php",
     "room" => "pages/users/room.php",
     "help" => "pages/users/help.php"
 ];
 
-// Cek apakah request cocok dengan route statis
+// Cek apakah request cocok route statis
 if (array_key_exists($request, $routes)) {
     include $routes[$request];
     exit;
 }
 
-// Tangani route dinamis: roomDetail/{type}
+// Route dinamis: roomDetail/{type}
 if (preg_match("#^roomDetail/([^/]+)$#", $request, $matches)) {
     $_GET['type'] = $matches[1];
     include "pages/users/roomDetail.php";
     exit;
 }
 
-// 404 jika tidak ditemukan
+// Jika tidak cocok, tampilkan 404
 http_response_code(404);
-echo "404 Not Found";
+echo "Tidak ditemukan Yaww";
